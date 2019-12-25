1d4f835a7db4e9a9026571349bd44503d29571dd
Dhruv Erry <54217858+dhruvErry@users.noreply.github.com>
2019-12-26 00:13:11 +0530

    Update uCard.java

--- uCard.java
+++ uCard.java
@@ -1,49 +1,49 @@
-public class uCard
+class uCard
 {
-    private int rank;
-    private char col;
-    private char pow;
-    public uCard(int rank, char col, char pow){
-        this.rank=rank;
-        this.col=col;
-        this.pow=pow;
+  private int rank;
+  private char col;
+  private char pow;
+  uCard(int rank, char col, char pow){
+    this.rank=rank;
+    this.col=col;
+    this.pow=pow;
+  }
+  int getRank(){
+    return rank;
+  }
+  char getCol(){
+    return col;
+  }
+  char getPow(){
+    return pow;
+  }
+  String printCard(){
+    String c="";
+    if(rank>-1)
+    c+="NUMBER: "+rank+"         ";
+    if(col==' '){
+      if(pow=='w')
+      c="POWER: Wild ";
+      if(pow=='D')
+      c="POWER: Draw Four ";
     }
-    public int getRank(){
-        return rank;
-    }
-    public char getCol(){
-        return col;
-    }
-    public char getPow(){
-        return pow;
-    }
-    public String toString(){
-        String c="";
-        if(rank>-1)
-            c+="NUMBER: "+rank+"\n";
-        String p="";
-        if(col=='r')
-            p="Red";
-        if(col=='g')
-            p="Green";
-        if(col=='b')
-            p="Blue";
-        if(col=='y')
-            p="Yellow";
-        c+="COLOUR: "+p+"\n";
-        if(col==' '){  
-            if(pow=='w')
-                c="POWER: Wild\n";
-            if(pow=='D')
-                c="POWER: Draw Four\n";
-        }
-        if(pow=='d')
-            c+="POWER: Draw Two\n";
-        if(pow=='r')
-            c+="POWER: Reverse\n";
-        if(pow=='s')
-            c+="POWER: Skip\n";
-        return c;
-    }
+    if(pow=='d')
+    c+="POWER: Draw Two   ";
+    if(pow=='r')
+    c+="POWER: Reverse    ";
+    if(pow=='s')
+    c+="POWER: Skip       ";
+    String p="";
+    if(col=='r')
+    p="Red";
+    if(col=='g')
+    p="Green";
+    if(col=='b')
+    p="Blue";
+    if(col=='y')
+    p="Yellow";
+    if(col!=' ')
+    c+="COLOUR: "+p+" ";
+    return c;
+  }
 }
-    
